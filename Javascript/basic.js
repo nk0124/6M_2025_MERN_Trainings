2fed0a7ac2f34317d84e2b0577277c222fd64834
nk0124 <160753539+nk0124@users.noreply.github.com>
2025-01-28 15:41:55 +0530

    basic.js

---
+++ Javascript/basic.js
@@ -0,0 +1,41 @@
+
+// for printout the statement
+
+console.log("hello world");
+
+var user="neha"
+var age=23;
+console.log(typeof(age));
+console.log(user ,age);
+
+var user="neha";
+var age="23";
+console.log(user+age);
+
+var color ="orange";
+console.log(color);
+
+console.log("color is" + color);
+
+var num=10
+
+console.log("Type of num" , typeof(num));
+
+console.log(color +"is worn by" , num ,"people");
+
+var a ="hello";
+console.log("type of hello", typeof(a));
+
+var b= "45";
+console.log(a+b);
+
+// white space are also count as a character 
+var b= " world";
+console.log(a+b);
+
+var y="hello  "
+console.log(y.length)
+
+console.log(color,"color is beautiful");
+
+console.log("code was" ,color);
