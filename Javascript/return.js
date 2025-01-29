85bbc900309566bd005ad63b002e4b9e924754e0
nk0124 <160753539+nk0124@users.noreply.github.com>
2025-01-29 19:34:37 +0530

    returnjs

---
+++ Javascript/return.js
@@ -0,0 +1,16 @@
+
+function remainder(num){
+    // console.log(num%2)
+    return num%2
+}
+
+let y=remainder(100)
+console.log(y,"y is")
+
+// =,-,*,/,%
+
+let rem=(num)=>{
+    return num%3
+}
+
+console.log(rem(35))
