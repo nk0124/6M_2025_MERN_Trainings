21d18e39288dd2d1c074eed734764081393c072e
nk0124 <160753539+nk0124@users.noreply.github.com>
2025-01-29 19:42:36 +0530

    ifjs

--- Javascript/var.js
+++ Javascript/var.js
@@ -12,3 +12,14 @@ var x= 10;
 
 x=20;
 console.log(x);
+
+
+function num(n){
+    if(n%2==0){
+        console.log("number is a even")
+    } else{
+        console.log("number is a odd ")
+    }
+}
+
+num(10);
