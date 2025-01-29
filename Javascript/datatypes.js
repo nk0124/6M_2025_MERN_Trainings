809048a1f2dd22265cfd31d61cd66f85bfb627d2
nk0124 <160753539+nk0124@users.noreply.github.com>
2025-01-29 18:41:21 +0530

    datatypes

---
+++ Javascript/datatypes.js
@@ -0,0 +1,47 @@
+
+
+// javascript has 8 datatypes 
+
+// string , number , bigint , Boolean
+// undefined , null , symbol , object
+
+
+// string
+let color ="pink";
+let name ="neetu";
+console.log(color,name);
+
+// number
+let length = 23;
+let weight = 7.4;
+console.log(length,weight);
+
+let x = 16 + "Volvo";
+console.log(x)
+
+let y = 16 + "Volvo";
+console.log(y);
+
+// Booleans
+let a = true;
+let b = false;
+console.log(a,b)
+
+let c; // value,type -> undefined
+console.log(c)
+console.log(typeof(c));
+c = 5;
+c = "Raj";
+console.log(c);
+
+let d=''; // value-> null , type -> string
+console.log(typeof(d));
+console.log(d);
+
+
+// Object:
+const person = {firstName:"Neha", lastName:"Kumari"};
+console.log(person)
+
+let e = BigInt("123456789012345678901234567890");
+console.log(e)
