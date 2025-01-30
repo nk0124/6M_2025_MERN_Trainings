6c36af17400e9268cc0e1e39ba1c9cead94d36e0
nk0124 <160753539+nk0124@users.noreply.github.com>
2025-01-30 18:04:26 +0530

    whilejs

---
+++ Javascript/while.js
@@ -0,0 +1,18 @@
+
+let i=1
+while(i<=5){
+    console.log(i)
+    i++
+}
+
+let k=1000
+while(k>=0){
+    console.log(k)
+    k-=100
+}
+
+// var i=1;
+// for(var ;i<=6;){
+//     console.log(i);
+//     i++;
+// }
