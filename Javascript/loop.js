0ca9c4c9f1228d8888b77a7b0c01933d30887cfc
nk0124 <160753539+nk0124@users.noreply.github.com>
2025-01-30 17:56:02 +0530

    loopjs

---
+++ Javascript/loop.js
@@ -0,0 +1,28 @@
+
+
+// for , while, do-wwhile
+
+// initialize - 1
+// condition  - 2
+// increment / decrement - 4
+
+// for(init;condition;inc/dec){
+// code
+//}
+
+
+// for(let i=1 i<=5;i++){ output- error
+//     console.log(i)
+// }  
+
+for(let i=1;i<=5;i++){
+    console.log(i)
+}
+// console.log(i) // output- error 
+
+
+for(var i=1;i<=5;i++){
+    console.log(i)
+}
+
+console.log(i) // 6
