b14ff9a6939b1b8b6580d02a6b52bbd0be93ca33
nk0124 <160753539+nk0124@users.noreply.github.com>
2025-02-02 11:11:55 +0530

    multiarrayjs

---
+++ Javascript/multiarray.js
@@ -0,0 +1,19 @@
+// if array and multiarray in both case that work with index all work 
+
+
+let students=[
+    ["dinkar","React","3months"],// index-0
+    ["aman","PHP","45days"],
+    ["Rahul","Node","3months"],
+]
+
+console.log(students)
+console.log(students[0][1]);
+console.log(students[2][0])
+students[2].push("June")
+console.log(students);
+students[0].pop()
+console.log(students)
+
+// json - javascript object notation
+// apis
