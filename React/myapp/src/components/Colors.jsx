97f1fbbd1eba3e66e32f269e2f9e659a34430f5e
nk0124 <160753539+nk0124@users.noreply.github.com>
2025-02-16 11:25:19 +0530

    Color component array

---
+++ React/myapp/src/components/Colors.jsx
@@ -0,0 +1,21 @@
+
+import { Fragment } from "react"
+export default function Colors(){
+    let colors=["red","green", "blue","pink","black"]
+    return(
+        <>
+            {colors.map((color,index)=>{
+                // console.log("hello", color, index);
+                return(
+                    <Fragment key={index}>
+                        <h1>{color}</h1>
+                        {/* <p>Hello</p> */}
+                    </Fragment>
+                )
+            })}
+        </>
+    )
+}
+
+
+
